67e36b7c03474def6d1b3d4196768b12e699034f
Fabio Pires <pf1437@fiap.com.br>
2026-09-03 12:25:33 +0000

    chore: Criado o arquivo security.py.

---
+++ config.py
@@ -0,0 +1,4 @@
+AWS_ACCESS_KEY_ID = "AKIA44EXAMPLE12345678"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+github_token = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"
+password = "MinhaSenhaSuperSecreta123!"
